835ce1d6dd285d8531d92b33042fb9954b743753
Stef Pletinck <stef.pletinck@ugent.be>
2025-12-05 13:04:10 +0100

    Day three part two

--- src/three.rs
+++ src/three.rs
@@ -6,22 +6,40 @@ use std::time::Instant;
 
 pub fn run() {
     let path = Path::new("data/three");
-    let file = File::open(&path).expect("Need input file");
-    let banks = io::BufReader::new(file).lines().map(|line| line.expect("WDYM its not a string"));
     {
+        let file = File::open(&path).expect("Need input file");
+        let banks = io::BufReader::new(file)
+            .lines()
+            .map(|line| line.expect("WDYM its not a string"));
         let start = Instant::now();
         let result = part_one(banks);
         let elapsed = start.elapsed();
         println!("Part one, result: {} , time: {:.2?}", result, elapsed);
     }
+    {
+        let file = File::open(&path).expect("Need input file");
+        let banks = io::BufReader::new(file)
+            .lines()
+            .map(|line| line.expect("WDYM its not a string"));
+        let start = Instant::now();
+        let result = part_two(banks);
+        let elapsed = start.elapsed();
+        println!("Part two, result: {} , time: {:.2?}", result, elapsed);
+    }
 }
 
 fn part_one<T: Iterator>(input: T) -> u32
-    where T::Item: ToString,
+where
+    T::Item: ToString,
 {
-    input
-        .map(|line| bank_max_joltage(&line.to_string()))
-        .sum()
+    input.map(|line| bank_max_joltage(&line.to_string())).sum()
+}
+
+fn part_two<T: Iterator>(input: T) -> u64
+where
+    T::Item: ToString,
+{
+    input.map(|x| max_for_n_digits(&x.to_string(), 12, 0)).sum()
 }
 
 fn bank_max_joltage(bank: &str) -> u32 {
@@ -37,7 +55,8 @@ fn bank_max_joltage(bank: &str) -> u32 {
             } else {
                 cmp
             }
-        }).expect("No biggest digit??");
+        })
+        .expect("No biggest digit??");
     let max_single = bank
         .chars()
         .skip(max_idx + 1)
@@ -46,6 +65,33 @@ fn bank_max_joltage(bank: &str) -> u32 {
         .expect("Need biggest single");
 
     max_dec * 10 + max_single
+}
+
+fn max_for_n_digits(bank: &str, n: usize, start_val: u64) -> u64 {
+    if n == 0 {
+        return start_val;
+    }
+
+    // Consider all the digits from 0..(bank.len() - n)
+    // Pick the first highest digit
+    // Recurse with the remaining substring
+    let (idx, digit) = bank
+        .char_indices()
+        .take(bank.len() - n + 1)
+        .map(|(idx, char)| (idx, char.to_digit(10).expect("Must be a digit") as u64))
+        .max_by(|x, y| {
+            let cmp = x.1.cmp(&y.1);
+            // max_by keeps the last one, so if theyre equal we keep the first one..
+            if cmp.is_eq() {
+                Ordering::Greater
+            } else {
+                cmp
+            }
+        })
+        .expect("We need a max");
+    let val: u64 = digit * 10_u64.pow(n as u32 - 1);
+    let substr = bank.chars().skip(idx + 1).collect::<String>();
+    max_for_n_digits(&substr, n - 1, start_val + val)
 }
 
 #[cfg(test)]
@@ -73,5 +119,13 @@ mod test {
     #[test]
     fn test_ordering() {
         assert_eq!(88, bank_max_joltage("878161111112111"));
+    }
+
+    #[test]
+    fn test_big_n() {
+        assert_eq!(987654321111, max_for_n_digits("987654321111111", 12, 0));
+        assert_eq!(811111111119, max_for_n_digits("811111111111119", 12, 0));
+        assert_eq!(434234234278, max_for_n_digits("234234234234278", 12, 0));
+        assert_eq!(888911112111, max_for_n_digits("818181911112111", 12, 0));
     }
 }
