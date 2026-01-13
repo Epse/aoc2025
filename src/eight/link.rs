1369e6bcb1b5650427597787f2d40bdb7c8bd21f
Stef Pletinck <stef.pletinck@ugent.be>
2026-01-13 14:52:12 +0100

    Ever so slightly speed up eight

--- src/eight/link.rs
+++ src/eight/link.rs
@@ -2,39 +2,34 @@ use super::Vector3;
 
 #[derive(Debug, PartialEq, Hash, Eq)]
 pub struct Link {
-    lesser: Vector3,
-    greater: Vector3,
+    first: Vector3,
+    second: Vector3,
 }
+
+// No longer does any guarantees about first and second for perf
 
 #[allow(dead_code)]
 impl Link {
     pub fn new(a: Vector3, b: Vector3) -> Self {
-        if a.abs() > b.abs() {
-            Self {
-                lesser: b,
-                greater: a,
-            }
-        } else {
-            Self {
-                lesser: a,
-                greater: b,
-            }
+        Self {
+            first: b,
+            second: a,
         }
     }
 
     pub fn contains(&self, other: &Vector3) -> bool {
-        self.lesser == *other || self.greater == *other
+        self.first == *other || self.second == *other
     }
 
     pub fn lesser(&self) -> &Vector3 {
-        &self.lesser
+        &self.first
     }
 
     pub fn greater(&self) -> &Vector3 {
-        &self.greater
+        &self.second
     }
 
     pub fn split(self) -> (Vector3, Vector3) {
-        (self.lesser, self.greater)
+        (self.first, self.second)
     }
 }
